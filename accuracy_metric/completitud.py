340c76eae78355c11b9f211d87088064be1ac8f5
Sandra Gomez Yagüez <sandra_yaguez@hotmail.com>
2016-04-25 14:24:42 +0200

    ultima version buena

--- accuracy_metric/completitud.py
+++ accuracy_metric/completitud.py
@@ -22,9 +22,13 @@ import re
 import ast
 import calendar
 import mixpanel
+import os
 from mixpanel import Mixpanel
 mp = Mixpanel("10b73632200abfbd592a5567ae99f065")
 import mixpanel_api
+
+#Abrir terminal
+#os.system("gnome-terminal -e 'bash -c \"sudo apt-get update; exec bash\"'")
 
 #JFS
 #CONSUMER_KEY= "7pBOCbidtVpQfTpPpwvQBL31o"
@@ -35,6 +39,7 @@ import mixpanel_api
 #---------------------------------------------------------------------------------------------------------------------
 #def main():
 network_list = ["twitter","instagram", "facebook", "github"]
+version_list = ["master","latency", "accuracy"]
 server_base_url = "http://localhost:8000"
 
 #de los comandos que ejecuto desde consola, me quedo con el segundo (posicion 1,array empieza en 0),consola: python completitud.py twitter coge la "variable" twitter
@@ -43,6 +48,11 @@ if len(sys.argv) >= 2:
 else:
     social_network = ''
 
+
+if len(sys.argv) >= 3:
+    version= sys.argv[2]
+else:
+    version = ''
 
 #CASOS:
 if social_network in network_list:
@@ -57,12 +67,17 @@ if social_network in network_list:
         ACCESS_KEY = '3072043347-T00ESRJtzlqHnGRNJZxrBP3IDV0S8c1uGIn1vWf' #Access token
         ACCESS_SECRET = 'OBPFI8deR6420txM1kCJP9eW59Xnbpe5NCbPgOlSJRock'   #Access token secret
         # Lanzamos una pestana por cada version del componente
-        webbrowser.open_new("http://twitter-timeline-app.appspot.com/app/TwitterCompletitud.html")
-        sleep(2)
-        #webbrowser.open_new("http://twitter-timeline-app.appspot.com/app/TwitterCompletitudLatency.html")
-        #sleep(2)
-        #webbrowser.open_new("http://twitter-timeline-app.appspot.com/app/TwitterCompletitudAccuracy.html")
-        #sleep(10)
+
+        if version in version_list:
+            if(version=="master"):
+                webbrowser.open_new("http://twitter-timeline-app.appspot.com/app/TwitterCompletitud.html")
+                sleep(5)
+            elif(version=="latency"):
+                webbrowser.open_new("http://twitter-timeline-app.appspot.com/app/TwitterCompletitudLatency.html")
+                sleep(5)
+            elif(version=="accuracy"):
+                webbrowser.open_new("http://twitter-timeline-app.appspot.com/app/TwitterCompletitudAccuracy.html")
+                sleep(5)
 
         oauth = OAuth1(CONSUMER_KEY,client_secret=CONSUMER_SECRET,resource_owner_key=ACCESS_KEY,resource_owner_secret=ACCESS_SECRET)
         request_hometimeline="https://api.twitter.com/1.1/statuses/home_timeline.json?count=200"
@@ -85,126 +100,136 @@ if social_network in network_list:
             #Twitter devuelve los id de los tweets cronologicamente (comprobado)
             ids.append(id_tweet1)
         print contador
+        zipPython=zip(ids,lis)
+        dictPython=dict(zipPython)
 
         ##########################################################################################################################################
         #-----------------------------------------DATOS TWITTER COMPONENTE (RECOGIDOS DE MIXPANEL)------------------------------------------------
         ##########################################################################################################################################
-
+sleep(10)
         # Hay que crear una instancia de la clase Mixpanel, con tus credenciales
-        x=mixpanel_api.Mixpanel("0be846115003ba87c667ee6467edb336","c282259a64f150a4ce2496a2dd73e097")
-        version_list = ["master","latency", "accuracy"]
-        lista=[]
-        listacomp=[]
-        listaid=[]
-        cont=0
+x=mixpanel_api.Mixpanel("0be846115003ba87c667ee6467edb336","c282259a64f150a4ce2496a2dd73e097")
+lista=[]
+listacomp=[]
+listaid=[]
+cont=0
 
-        if len(sys.argv) >= 3:
-            version= sys.argv[2]
-        else:
-            version = ''
+if version in version_list:
+    if version=="master":
+        #defino los parametros necesarios para la peticion
+        params={'event':"master",'name':'value','type':"general",'unit':"day",'interval':1}
+        respuesta=x.request(['events/properties/values'], params, format='json')
 
-        for version in version_list:
-            print version
-            if(version=="master"):
-            #defino los parametros necesarios para la peticion
-                params={'event':"master",'name':'value','type':"general",'unit':"day",'interval':1}
-                respuesta=x.request(['events/properties/values'], params, format='json')
+        for x in respuesta:
+            #pasar de unicode a dict
+            resp = ast.literal_eval(x)
+            lista.append(resp)
 
-                for x in respuesta:
+        #ordeno la lista de diccionarios por el id
+        newlist = sorted(lista, key=lambda id_tweet: id_tweet['id'])
+        newlist.reverse()
+
+        for y in newlist:
+            #la k son los text y el id y las v son los valores de cada uno. [0][1] del texto cojo su valor (posicion 0 que es el texto y posicion 1 que es el valor)
+            textocomp=y.items()[0][1]
+            idcomp=y.items()[1][1]
+            idcomp=int(idcomp)
+            listacomp.append(textocomp)
+            listaid.append(idcomp)
+        
+        zipComp=zip(listaid,listacomp)
+        dictComp=dict(zipComp)
+
+        cont=1
+        #Recorro el diccionario del componente
+        for key,value in dictComp.iteritems():
+            #compruebo que el diccionario de Python contiene todas las claves del diccionario del componente
+            if(dictPython.has_key(key)):
+                #si es asi, cojo los values de python y del componente y los comparo
+                cont+=1
+                valuesP=dictPython.get(key,None)
+                if cmp(valuesP,value)==0:
+                    print "OK"
+                else:
+                    print "falla en: " + n
+            else:
+                print "falla en: " + str(key)
+                print "el tweet es: " + value
+                mp.track(cont,"Fallos master",{"posicion": listacomp.index(value)+1 , "tweet": value, "version":version})
+        print cont        
+
+            
+    elif version=="latency":
+                #defino los parametros necesarios para la peticion
+        params={'event':"latency",'name':'value','type':"general",'unit':"day",'interval':1}
+        respuesta=x.request(['events/properties/values'], params, format='json')
+
+        for x in respuesta:
                     #pasar de unicode a dict
-                    resp = ast.literal_eval(x)
-                    lista.append(resp)
+            resp = ast.literal_eval(x)
+            lista.append(resp)
 
                 #ordeno la lista de diccionarios por el id
-                newlist = sorted(lista, key=lambda id_tweet: id_tweet['id'])
-                newlist.reverse()
+        newlist = sorted(lista, key=lambda id_tweet: id_tweet['id'])
+        newlist.reverse()
+
+        for y in newlist:
+                #la k son los text y el id y las v son los valores de cada uno. [0][1] del texto cojo su valor (posicion 0 que es el texto y posicion 1 que es el valor)
+            textocomp=y.items()[0][1]
+            idcomp=y.items()[1][1]
+            idcomp=int(idcomp)
+            listacomp.append(textocomp)
+            listaid.append(idcomp)
+
+        for k,v in zip(lis,listacomp):
+                #la k son los tweets obtenidos de twitter y v son los tweets obtenidos del componente
+            if cmp(k,v)==0:
+                cont+=1
+                print "OK"
+            else:
+                print "NO completitud"
+                cont+=1
+                print "falla en el numero: " + str(cont)
+                    #con listacom.index(v) mando a mixpanel la posicion en la que esta el tweet que fall en el componente
+                mp.track(cont,"Fallos latency",{"posicion": listacomp.index(v)+1 , "tweet": v, "version":version})
+
+    elif version=="accuracy":
+                     #defino los parametros necesarios para la peticion
+        params={'event':"accuracy",'name':'value','type':"general",'unit':"day",'interval':1}
+        respuesta=x.request(['events/properties/values'], params, format='json')
+
+        for x in respuesta:
+                    #pasar de unicode a dict
+            resp = ast.literal_eval(x)
+            lista.append(resp)
+
+                #ordeno la lista de diccionarios por el id
+        newlist = sorted(lista, key=lambda id_tweet: id_tweet['id'])
+        newlist.reverse()
+
+        for y in newlist:
+                #la k son los text y el id y las v son los valores de cada uno. [0][1] del texto cojo su valor (posicion 0 que es el texto y posicion 1 que es el valor)
+            textocomp=y.items()[0][1]
+            idcomp=y.items()[1][1]
+            idcomp=int(idcomp)
+            listacomp.append(textocomp)
+            listaid.append(idcomp)
+
+        for k,v in zip(lis,listacomp):
+                #la k son los tweets obtenidos de twitter y v son los tweets obtenidos del componente
+            if cmp(k,v)==0:
+                cont+=1
+                print "OK"
+
+            else:
+                print "NO completitud"
+                cont+=1
+                print "falla en el numero: " + str(cont)
+                        #con listacom.index(v) mando a mixpanel la posicion en la que esta el tweet que fall en el componente
+                mp.track(cont,"Fallos accuracy",{"posicion": listacomp.index(v)+1 , "tweet": v, "version":version})
+
+
                 
-                for y in newlist:
-                #la k son los text y el id y las v son los valores de cada uno. [0][1] del texto cojo su valor (posicion 0 que es el texto y posicion 1 que es el valor)
-                    textocomp=y.items()[0][1]
-                    idcomp=y.items()[1][1]
-                    idcomp=int(idcomp)
-                    listacomp.append(textocomp)
-                    listaid.append(idcomp)
-
-                for k,v in zip(lis,listacomp):
-                #la k son los tweets obtenidos de twitter y v son los tweets obtenidos del componente
-                    if cmp(k,v)==0:
-                        cont+=1       
-                    else:
-                        print "-----------------------------------------------------"
-                        print "NO completitud"
-                        cont+=1
-                        print "falla en el numero: " + str(cont)
-                        #con listacom.index(v) mando a mixpanel la posicion en la que esta el tweet que fall en el componente
-                        mp.track(cont,"Fallos master",{"posicion": listacomp.index(v)+1 , "tweet": v, "version":version})
-
-            elif(version=="latency"):
-                     #defino los parametros necesarios para la peticion
-                params={'event':"latency",'name':'value','type':"general",'unit':"day",'interval':1}
-                respuesta=x.request(['events/properties/values'], params, format='json')
-
-                for x in respuesta:
-                    #pasar de unicode a dict
-                    resp = ast.literal_eval(x)
-                    lista.append(resp)
-
-                #ordeno la lista de diccionarios por el id
-                newlist = sorted(lista, key=lambda id_tweet: id_tweet['id'])
-                newlist.reverse()
-
-                for y in newlist:
-                #la k son los text y el id y las v son los valores de cada uno. [0][1] del texto cojo su valor (posicion 0 que es el texto y posicion 1 que es el valor)
-                    textocomp=y.items()[0][1]
-                    idcomp=y.items()[1][1]
-                    idcomp=int(idcomp)
-                    listacomp.append(textocomp)
-                    listaid.append(idcomp)
-
-                for k,v in zip(lis,listacomp):
-                #la k son los tweets obtenidos de twitter y v son los tweets obtenidos del componente
-                    if cmp(k,v)==0:
-                        cont+=1
-                    else:
-                        print "NO completitud"
-                        cont+=1
-                        print "falla en el numero: " + str(cont)
-                        #con listacom.index(v) mando a mixpanel la posicion en la que esta el tweet que fall en el componente
-                        mp.track(cont,"Fallos latency",{"posicion": listacomp.index(v)+1 , "tweet": v, "version":version})
-
-            elif(version=="accuracy"):
-                     #defino los parametros necesarios para la peticion
-                params={'event':"accuracy",'name':'value','type':"general",'unit':"day",'interval':1}
-                respuesta=x.request(['events/properties/values'], params, format='json')
-
-                for x in respuesta:
-                    #pasar de unicode a dict
-                    resp = ast.literal_eval(x)
-                    lista.append(resp)
-
-                #ordeno la lista de diccionarios por el id
-                newlist = sorted(lista, key=lambda id_tweet: id_tweet['id'])
-                newlist.reverse()
-
-                for y in newlist:
-                #la k son los text y el id y las v son los valores de cada uno. [0][1] del texto cojo su valor (posicion 0 que es el texto y posicion 1 que es el valor)
-                    textocomp=y.items()[0][1]
-                    idcomp=y.items()[1][1]
-                    idcomp=int(idcomp)
-                    listacomp.append(textocomp)
-                    listaid.append(idcomp)
-
-                for k,v in zip(lis,listacomp):
-                #la k son los tweets obtenidos de twitter y v son los tweets obtenidos del componente
-                    if cmp(k,v)==0:
-                        cont+=1
-                    else:
-                        print "NO completitud"
-                        cont+=1
-                        print "falla en el numero: " + str(cont)
-                        #con listacom.index(v) mando a mixpanel la posicion en la que esta el tweet que fall en el componente
-                        mp.track(cont,"Fallos accuracy",{"posicion": listacomp.index(v)+1 , "tweet": v, "version":version})
-
         # elif social_network == 'github':
 
         #     webbrowser.open_new(server_base_url + "/Stable/GithubEventsLatency.html?experiment="+ experiment_id)
@@ -217,10 +242,10 @@ if social_network in network_list:
         #     access_token = sys.argv[2]
             
     
-    else:
-        print "Wrong social network or missing param"
+    #else:
+        #print "Wrong social network or missing param"
         # {}: Obligatorio, []: opcional
-        print "Usage: completitud.py {twitter|facebook|instagram|github [facebook_access_token]"
+        #print "Usage: completitud.py {twitter|facebook|instagram|github [facebook_access_token]"
 
 
 #if __name__ == "__main__":
