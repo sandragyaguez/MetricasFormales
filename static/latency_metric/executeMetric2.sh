d08142dd74c3623a9096309d8646dcda16622bf3
lruizr <l.ruizr04@gmail.com>
2016-12-09 14:01:58 +0100

    Changes made to calculate metrics

--- static/latency_metric/executeMetric2.sh
+++ static/latency_metric/executeMetric2.sh
@@ -8,8 +8,8 @@
 # Antes de hacer una prueba nueva hay que hacer los siguientes pasos:
 # - 	Hay que cambiar el token que está en FacebookWallLatency.html (carpeta Stable) y la variable FACEBOOK_TOKEN
 # En GoogleplusLatency.html (Carpetas Accuracy, Latency, Stable) cambiar el valor de la variable access_token y la variable GOOGLE_TOKEN
-FACEBOOK_TOKEN="EAANMUmJPs2UBAMihfYZAXEoOnaqxkigZCMQb9xewYQxhbUOmdjb7p25n6X8K301o1OzYxYDsHfGcqtBZBBcMuNEhZCpViFqV8ndvBgxZArsR9s78K4CKZCkE4cjCImJ6qjVSaqOG37TykZBEzqbKzfwmU8ZCYjv3b16NLss1QjV8iwZDZD"
-GOOGLE_TOKEN="ya29.CjlZA16oU6ZNa3JEj5JTNG3I4_gj89sHFG0gjSD4O-7_XNTJ0z83CHV4-JCCh0HZBOyRR6ipFQ0Oi9Q"
+FACEBOOK_TOKEN="EAACEdEose0cBAD1OiaE2nKOKXMD3zW7CfhZBD2npN2Hi6ZBb26IcDJ1EqQaUKsWiZC7E894BSZBlJEKGvw0iZBGDiDmZAeLMnDTwNXAOJsZAY5uYq0qrf5agYJpNHONPlPhYhEsJsgXo4uDZBzKo5y8xZCbU3jQI2jiijpbB2DTmzzwZDZD"
+GOOGLE_TOKEN="ya29.CjmvA8NQfO9YjPqSyrK27bbkwiD2y_Jq22Ypjr8PfExjQhN9_wRygRWmwatzXUFi719uJsl7jeeWZWo"
 
 # Comentar esta línea si los componentes están deplegados en remoto
 python -m SimpleHTTPServer >> /dev/null &
@@ -22,10 +22,10 @@ echo $PID
 
 # Esperamos un tiempo para asegurar que se recolectan los eventos de las distintas versiones
 # y para intentar que las ejecuciones anteriores de componentes "no afecten" posteriores medidas
-sleep 10
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente github-events..."
-python measureLatency.py github
+# sleep 10
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente github-events..."
+# python measureLatency.py github
 
 sleep 10
 echo "##################################################################"
@@ -33,19 +33,19 @@ echo "Realizando pruebas sobre el componente facebook-wall..."
 python measureLatency.py facebook $FACEBOOK_TOKEN
 
 #lo tengo en latency_metric_ana por falta de espacion al desplegar en app engine
-#sleep 10
-#echo "##################################################################"
-#echo "Realizando pruebas sobre el componente googleplus-timeline..."
-#python measureLatency.py googleplus $GOOGLE_TOKEN
+sleep 10
+echo "##################################################################"
+echo "Realizando pruebas sobre el componente googleplus-timeline..."
+python measureLatency.py googleplus $GOOGLE_TOKEN
 
 
 sleep 10
 echo "##################################################################"
 echo "Recolectando y calculando métrica de latencia sobre los componentes probados..."
 #python collectLatencyRecords.py instagram-timeline
-python collectLatencyRecords.py github-events
+#python collectLatencyRecords.py github-events
 python collectLatencyRecords.py facebook-wall
-#python collectLatencyRecords.py googleplus-timeline
+python collectLatencyRecords.py googleplus-timeline
 echo "Métricas calculadas"
 
 # Matamos el proceso correspondiente al servidor local de componentes de python
