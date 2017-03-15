584b37863ca24729d6a991286cec4efff22821e4
lruizr <l.ruizr04@gmail.com>
2017-03-15 13:11:48 +0100

    Fixed bugs

--- static/latency_metric/collectLatencyRecords.py
+++ static/latency_metric/collectLatencyRecords.py
@@ -16,8 +16,8 @@ mp = Mixpanel("53da31965c3d047fa72de756aae43db1")
 
 # We set the date to today
 #START_DATE = time.strftime("%Y-%m-%d")
-START_DATE = "2017-03-15"
-END_DATE = "2017-03-15"
+START_DATE = time.strftime("%Y-%m-%d")
+END_DATE = START_DATE
 # La fecha desde la que has empezado a enviar datos a Mixpanel (se usa para ver si hay algún resultado final que pueda estar duplicado)
 START_STUDY_DATE = "2017-03-15"
 
