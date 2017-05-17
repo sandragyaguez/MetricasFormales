84e20f328e862db85cf5acd7569f4e9d1dde0759
lruizr <l.ruizr04@gmail.com>
2017-05-17 18:19:31 +0200

    Commented components to execute one by one

--- static/accuracy_metric/accuracy_metric.sh
+++ static/accuracy_metric/accuracy_metric.sh
@@ -11,23 +11,22 @@
 #TWITTER
 #---------#
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente twitter-timeline: metrica completitud version master"
-python completitud_final.py twitter master
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente twitter-timeline: metrica completitud version master"
+# python completitud_final.py twitter master
+# sleep 10
+# # pkill chrome
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente twitter-timeline: metrica completitud version latency"
-python completitud_final.py twitter latency
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente twitter-timeline: metrica completitud version accuracy"
+# python completitud_final.py twitter accuracy
+# sleep 10
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente twitter-timeline: metrica completitud version accuracy"
-python completitud_final.py twitter accuracy
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente twitter-timeline: metrica completitud version latency"
+# python completitud_final.py twitter latency
+# sleep 10
+# pkill chrome
 
 #---------#
 #GITHUB
@@ -79,91 +78,117 @@ pkill chrome
 #FACEBOOK
 #---------#
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente facebook-wall: metrica completitud version master"
-python completitud_final.py facebook master
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente facebook-wall: metrica completitud version master"
+# python completitud_final.py facebook master
+# sleep 10
+# # pkill chrome
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente facebook-wall: metrica completitud version latency"
-python completitud_final.py facebook latency
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente facebook-wall: metrica completitud version accuracy"
+# python completitud_final.py facebook accuracy
+# sleep 10
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente facebook-wall: metrica completitud version accuracy"
-python completitud_final.py facebook accuracy
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente facebook-wall: metrica completitud version latency"
+# python completitud_final.py facebook latency
+# sleep 10
+# pkill chrome
 
 
 #---------#
 #GOOGLE+
 #---------#
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente googleplus-timeline: metrica completitud version master"
-python completitud_final.py googleplus master
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente googleplus-timeline: metrica completitud version master"
+# python completitud_final.py googleplus master
+# sleep 10
+# # pkill chrome
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente googleplus-timeline: metrica completitud version latency"
-python completitud_final.py googleplus latency
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente googleplus-timeline: metrica completitud version accuracy"
+# python completitud_final.py googleplus accuracy
+# sleep 10
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente googleplus-timeline: metrica completitud version accuracy"
-python completitud_final.py googleplus accuracy
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente googleplus-timeline: metrica completitud version latency"
+# python completitud_final.py googleplus latency
+# sleep 10
+# pkill chrome
 
 #-------#
 # PINTEREST
 #-------#
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente pinterest-timeline: metrica completitud version master"
-python completitud_final.py pinterest master
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente pinterest-timeline: metrica completitud version master"
+# python completitud_final.py pinterest master
+# sleep 10
+# # pkill chrome
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente pinterest-timeline: metrica completitud version latency"
-python completitud_final.py pinterest latency
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente pinterest-timeline: metrica completitud version accuracy"
+# python completitud_final.py pinterest accuracy
+# sleep 10
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente pinterest-timeline: metrica completitud version accuracy"
-python completitud_final.py pinterest accuracy
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente pinterest-timeline: metrica completitud version latency"
+# python completitud_final.py pinterest latency
+# sleep 10
+# # pkill chrome
 
 
 ##############################
 ########### STOCK ############
 ##############################
 
-echo "##################################################################"
-echo "Realizando pruebas sobre el componente finance-search: metrica completitud version master"
-python completitud_final.py finance-search master
-sleep 10
-pkill chrome
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente finance-search: metrica completitud version master"
+# python completitud_final.py finance-search master
+# sleep 10
+# # pkill chrome
+
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente finance-search: metrica completitud version accuracy"
+# python completitud_final.py finance-search accuracy
+# sleep 10
+# pkill chrome
+
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente finance-search: metrica completitud version latency"
+# python completitud_final.py finance-search latency
+# sleep 10
+# pkill chrome
+
+
+#############################
+######## WEATHER ############
+#############################
 
 echo "##################################################################"
-echo "Realizando pruebas sobre el componente finance-search: metrica completitud version latency"
-python completitud_final.py finance-search latency
+echo "Realizando pruebas sobre el componente open-weather: metrica completitud version master"
+python completitud_final.py open-weather master
 sleep 10
-pkill chrome
 
 echo "##################################################################"
-echo "Realizando pruebas sobre el componente finance-search: metrica completitud version accuracy"
-python completitud_final.py finance-search accuracy
+echo "Realizando pruebas sobre el componente open-weather: metrica completitud version accuracy"
+python completitud_final.py open-weather accuracy
 sleep 10
-pkill chrome
+
+#############################
+######## TRAFFIC ############
+#############################
+
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente traffic-incidents: metrica completitud version master"
+# python completitud_final.py traffic-incidents master
+# sleep 10
+
+# echo "##################################################################"
+# echo "Realizando pruebas sobre el componente traffic-incidents: metrica completitud version accuracy"
+# python completitud_final.py traffic-incidents accuracy
+# sleep 10
 
 echo "##################################################################"
 echo "##################################################################"
