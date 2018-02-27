ea4d2a6b3e247171961b499f267a157316a68700
Sandra Gomez Yagüez <sandra_yaguez@hotmail.com>
2018-02-27 13:23:29 +0100

    refresh metric finished for master version

--- static/refresh_metric/Master/spotify-component/SpotifyRefresco.js
+++ static/refresh_metric/Master/spotify-component/SpotifyRefresco.js
@@ -4,6 +4,7 @@ document.addEventListener('WebComponentsReady', function () {
   var publish_text = window.location.search;
   //desplazo en uno para quitar la interrogacion y guardo desde ahi hasta el final del texto publicado
   publish_text = publish_text.slice(1, publish_text.length);
+  var publicado=false;
   window.setTimeout(function () {
     //id del primer elemento que muestra el timeline
     //var last_element_id = element.res[0].id;
@@ -12,69 +13,31 @@ document.addEventListener('WebComponentsReady', function () {
       var found = false;
       var time;
       if (element.res) {
-        for (var i = 0; i < element.res.length && !found; i++) {
+        for (var i = 0; i < element.res.length && !found && !publicado; i++) {
           data = element.res[i].name;
-          //element.addEventListener('infoPista-changed', function (event) {
-
-            if (element.res[i].infoPista != undefined) {
+          if (element.res[i].infoPista != undefined) {
             for (var j = 0; j < element.res[i].infoPista.length; j++) {
               uris = element.res[i].infoPista[j].track.uri;
               data += uris;
             }
-            }
-            if (element.res[i].name == publish_text) {
-              found = true;
-              time = new Date().getTime();
-            }
-         // });
+          }
+          if (publish_text == data) {
+            found = true;
+            time = new Date().getTime();
+            var diccionario = {
+              'time': time,
+              'post': data
+            };
+            var dicc_string = JSON.stringify(diccionario);
+            mixpanel.track("master", {
+              'value': dicc_string,
+            });
+            publicado=true;
+          }
         }
-
-        if (found) {
-          var diccionario = {
-            'time': time,
-            "post": publish_text
-          };
-          var dicc_string = JSON.stringify(diccionario);
-          mixpanel.track("master", {
-            'value': dicc_string,
-          });
-        }
-
       } else {
         console.log("Se intenta acceder sin haber res");
       }
     });
   }, 8000);
 });
-
-//   if (event.detail) {
-  //     var haymas = true;
-  //     //voy a recorrer los nuevos cambios y comprobar si el event es el que queria
-  //     for (var i = 0; i < element.res.length && haymas; i++) {
-  //       data = element.res[i].name;
-  //       if (element.res[i].infoPista != undefined) {
-  //         for (var j = 0; j < element.res[i].infoPista.length; j++) {
-  //           uris = element.res[i].infoPista[j].track.uri;
-  //           data += uris;
-  //         }
-  //       }
-  //       if (element.res[i].id === last_element_id) {
-  //         haymas = false;
-  //         console.log("ha entrado")
-  //         //cuando no haya mas, tengo que actualizar el id al primero de la pila
-  //         //en last_element_id tengo el id del post que voy a comparar con el id obtenido de python para comprobar que lo que se refresca en el componente es lo mismo que he postead
-  //         last_element_id = element.res[0].id;
-  //       }
-  //       else {
-  //         if ((data === publish_text)) {
-  //           var diccionario = {
-  //             'time': time,
-  //             'post': publish_text
-  //           }
-  //           var dicc_string = JSON.stringify(diccionario);
-  //           mixpanel.track("master", { 'value': dicc_string });
-  //           console.log("time de escucha de cambio en ms: " + time);
-  //         }
-  //       }
-  //     }
-  //   }
