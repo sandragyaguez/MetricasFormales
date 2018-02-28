e0fb547d7e9dafa3574ca5487d046a927da3d4b5
Alvaro <alvaro.hernangomez.jimenez@alumnos.upm.es>
2018-02-28 12:43:03 +0100

    Added graphs of days and months in graficasUptime.py

--- static/uptime_metric/graficasUptime.py
+++ static/uptime_metric/graficasUptime.py
@@ -4,6 +4,7 @@
 import matplotlib.pyplot as plt
 import time
 import os
+import sys
 from sys import argv
 from pymongo import Connection
 import matplotlib.dates as md
@@ -13,18 +14,38 @@ path = os.path.dirname(os.path.abspath(__file__))
 con = Connection('localhost')
 db = con.StatusAPIs
 
-script, app = argv
-
-t = time.time()
-
-####################################     BUSQUEDA POR LAS ULTIMAS 24 HORAS      #################################
+script, app, medida = argv
 
 tuplas = []
 
-for i in range(24):
+horaActual = time.time()
+
+if medida == "horas":
+    segundosARestar = 3600
+    rango = range(24)
+    formato = '%H:%M'
+    caracter = 'a'
+
+elif medida == "dias":
+    segundosARestar = 86400
+    rango = range(30)
+    formato = '%D'
+    caracter = 'o'
+
+elif medida == "meses":
+    segundosARestar = 2592000
+    rango = range(12)
+    formato = '%m:%Y'
+    caracter = 'o'
+else:
+    print "La medida para medir no es correcta."
+    con.close()
+    sys.exit()
+
+for i in rango:
     lineas_totales = 0
     lineas_correctas = 0
-    for element in db[app].find({ "time": { "$gt": t-3600, "$lt": t } } ):
+    for element in db[app].find({ "time": { "$gt": horaActual-segundosARestar, "$lt": horaActual } } ):
         if element['code'] == 200:
             lineas_correctas += 1
         lineas_totales += 1
@@ -32,16 +53,16 @@ for i in range(24):
         porcentaje = 0.0
     else:
         porcentaje = lineas_correctas / float(lineas_totales) * 100
-    t = t-3600
-    tuplas.insert(0, (dt.datetime.fromtimestamp(int(t)),porcentaje))
+    horaActual = horaActual-segundosARestar
+    tuplas.insert(0, (dt.datetime.fromtimestamp(int(horaActual)),porcentaje))
 
-ax=plt.gca()
-xfmt = md.DateFormatter('%H:%M')
-ax.xaxis.set_major_formatter(xfmt)
-plt.plot(*zip(*tuplas))
-
-plt.xlabel('Horas')
+plt.xlabel(medida)
 plt.ylabel('Porcentaje')
-plt.title('Uptime de las ultimas 24 horas de \"' + app + '\"')
+plt.title('Uptime de l' + caracter + 's ultim' + caracter + 's ' + str(i+1) + ' ' + medida + ' de \"' + app + '\"')
+ax=plt.gca()
+xfmt = md.DateFormatter(formato)
+ax.xaxis.set_major_formatter(xfmt)
+
+plt.plot(*zip(*tuplas))
 plt.show()
 con.close()
